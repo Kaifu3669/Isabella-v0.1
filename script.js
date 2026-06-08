53c30fab2745fba7c00d2177ceb10bcc42ec1786
Nobody <lolpopsingh757@gmail.com>
2026-06-09 01:40:29 +0530

    Add memory save and recall functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let memory =
+JSON.parse(
+localStorage.getItem("memory")
+) || {};
+
 function sendMessage(){
 
   let input =
@@ -8,6 +13,26 @@ function sendMessage(){
 
   if(message === ""){
     return;
+  }
+
+  /* MEMORY SAVE */
+
+  if(
+    message.toLowerCase().startsWith(
+      "my name is "
+    )
+  ){
+
+    let name =
+    message.substring(11).trim();
+
+    memory.name = name;
+
+    localStorage.setItem(
+      "memory",
+      JSON.stringify(memory)
+    );
+
   }
 
   let chatBox =
@@ -21,11 +46,35 @@ function sendMessage(){
 
   input.value = "";
 
+  let reply =
+  "I heard you. Tell me more.";
+
+  /* MEMORY RECALL */
+
+  if(
+    message.toLowerCase() ===
+    "what is my name"
+  ){
+
+    if(memory.name){
+
+      reply =
+      `Your name is ${memory.name}.`;
+
+    }else{
+
+      reply =
+      "I don't know your name yet.";
+
+    }
+
+  }
+
   setTimeout(()=>{
 
     chatBox.innerHTML += `
       <div class="botMsg">
-        I heard you. Tell me more.
+        ${reply}
       </div>
     `;
 
